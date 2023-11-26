5e736f99a8deb1c563bf1e4ff4f921e2b4be2797
jogony <seonwongim@gmail.com>
2023-11-26 21:57:16 +0900

    BOJ_16236 - 구현 + BFS (작업중)

--- Algorithm/src/bfs/BOJ_16236.java
+++ Algorithm/src/bfs/BOJ_16236.java
@@ -1,9 +1,7 @@
 package bfs;
 
 import java.io.*;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class BOJ_16236 {
 
@@ -12,14 +10,14 @@ public class BOJ_16236 {
     public static int[][] map;
     public static boolean[][] visited;
 
-    private static Queue<int[]> q;
+    private static Queue<Fish> q;
 
-    private static int[] dx = {1, -1, 0, 0};
-    private static int[] dy = {0, 0, -1, 1};
+    private static int[] dx = {-1, 0, 1, 0};
+    private static int[] dy = {0, -1, 0, 1};
 
     public static void main(String[] args) throws IOException {
 
-        BabyShark babyShark = new BabyShark();
+        Fish fish = null;
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -27,129 +25,108 @@ public class BOJ_16236 {
 
         N = Integer.parseInt(br.readLine());
 
-        map    = new int[N+1][N+1];
-        visited = new boolean[N+1][N+1];
+        List<Fish> list = new ArrayList<>();
+        q = new LinkedList<>();
 
-        for(int i = 0; i < N; i++) {
+        map = new int[N+1][N+1];
+
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
 
-            for(int j = 0; j < N; j++) {
+            for (int j = 0; j < N; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
 
-                if(map[i][j] == 9) {
-                    babyShark.x = i;
-                    babyShark.y = j;
+                if (map[i][j] == 9) {
+                    fish = new Fish(i, j, 2, 0, 0);
                     map[i][j] = 0;
                 }
+
+                if (map[i][j] == 1) {
+                    list.add(new Fish(i, j, 1, 0, 0));
+                }
             }
+        }
+
+        if (list.isEmpty()) {
+            System.out.println(0);
+            return;
         }
 
         br.close();
 
-        //targeting(babyShark);
-
-        while (targeting(babyShark)) {
-            TIME += bfs(babyShark);
-        }
-
-        System.out.println(TIME);
-    }
-
-    public static int bfs(BabyShark babyShark) {
-        q = new LinkedList<>();
-        q.add(new int[] {babyShark.x, babyShark.y, 0});
-
-        TIME = 0;
-        while (!q.isEmpty()) {
-            int nx = q.peek()[0];
-            int ny = q.peek()[1];
-            int cnt = q.peek()[2];
-
-            q.poll();
-
-            for(int i = 0; i < 4; i++) {
-
-                int curx = nx + dx[i];
-                int cury = ny + dy[i];
-                int curc = cnt + 1;
-
-                if(curx < 0 || cury < 0 || curx > N || cury > N) continue;
-
-                if(map[curx][cury] > babyShark.height) continue;
-
-                if(curx == babyShark.targetX && cury == babyShark.targetY) {
-                    babyShark.eat();
-                    map[curx][cury] = 0;
-
-                    babyShark.x = curx;
-                    babyShark.y = cury;
-                    babyShark.targetD = 1000;
-
-                    visited[curx][cury] = true;
-                    return curc;
-                }
-
-                if(map[curx][cury] == 0 || map[curx][cury] == babyShark.height) {
-                    q.add(new int[] {curx, cury, curc});
-                }
-            }
-        }
-        return 0;
-    }
-
-    public static boolean targeting(BabyShark shark) {
-        boolean result = false;
-
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < N; j++) {
-                if(!visited[i][j] && map[i][j] < shark.height && map[i][j] != 0) {
-                    shark.think(i, j);
-                    result = true;
-                }
-            }
-        }
-
-        return result;
+        bfs(fish);
     }
 
 
-    public static class BabyShark {
-        public int height = 2;
-        public int cnt = 0;
+    public static void bfs(Fish shark) {
 
-        public int x;
-        public int y;
-        public int targetX = 21;
-        public int targetY = 21;
-        public int targetD = 1000;
+        Queue<Fish> que = new PriorityQueue<>(Comparator
+                .comparingInt((Fish f) -> f.time)
+                .thenComparingInt(f -> f.x)
+                .thenComparingInt(f -> f.y));
 
-        public void eat() {
-            cnt++;
-            if(height == cnt) {
-                cnt = 0;
-                height++;
+        q.offer(shark);
+
+        while (true) {
+            visited = new boolean[N+1][N+1];
+            visited[q.peek().x][q.peek().y] = true;
+
+            while (!q.isEmpty()) {
+                Fish curF = q.poll();
+
+                for (int i = 0; i < 4; i++) {
+                    int nx = curF.x + dx[i];
+                    int ny = curF.y + dy[i];
+
+                    if (nx < 0 || ny < 0 || nx >= N || ny >= N ||
+                        visited[nx][ny] || map[nx][ny] > shark.size) {
+                        continue;
+                    }
+
+                    if (map[nx][ny] < shark.size && map[nx][ny] != 0) {
+                        que.offer(new Fish(nx, ny, curF.size, curF.eatCnt + 1, curF.time + 1));
+                    }
+
+                    q.offer(new Fish(nx, ny, curF.size, curF.eatCnt, curF.time + 1));
+                    visited[nx][ny] = true;
+                }
             }
+
+            if (que.isEmpty()) {
+                System.out.println(TIME);
+                return;
+            }
+
+            Fish fish = que.poll();
+            if (fish.size == fish.eatCnt) {
+                fish.size++;
+                fish.eatCnt = 0;
+            }
+
+            map[fish.x][fish.y] = 0;
+            TIME += fish.time;
+
+            System.out.println(fish.x + " " + fish.y);
+            System.out.println("size : " + fish.size + " eatCnt : " + fish.eatCnt);
+
+            q.offer(new Fish(fish.x, fish.y, fish.size, fish.eatCnt, 0));
+            que.clear();
         }
+    }
 
+    public static class Fish {
+        int x;
+        int y;
+        int size;
+        int eatCnt;
+        int time;
 
-        public void think(int i, int j) {
-            int distant = Math.abs(i-x) + Math.abs(j-y);
-            if(distant < this.targetD) {
-                targetX = i;
-                targetY = j;
-                targetD = distant;
-            } else if (distant == this.targetD) {
-                process(i, j);
-            }
-        }
-
-        public void process(int i, int j) {
-            if (targetX > i) {
-                targetX = i;
-                targetY = j;
-            } else if ( targetY > j) {
-                targetY = j;
-            }
+        public Fish(int x, int y, int size, int eatCnt, int time) {
+            this.x = x;
+            this.y = y;
+            this.size = size;
+            this.eatCnt = eatCnt;
+            this.time = time;
         }
     }
 }
