decce74fb7b6fbbf5a10a7afb17646ee86efa1d5
jogony <seonwongim@gmail.com>
2023-12-01 01:35:28 +0900

    BOJ_16234 - 구현 + BFS(구현중) 풀긴 했는데 좀 비효울적인 것 같다 추후 수정해보기

--- Algorithm/src/bfs/BOJ_16234.java
+++ Algorithm/src/bfs/BOJ_16234.java
@@ -4,7 +4,6 @@ import java.io.*;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
-import java.util.concurrent.LinkedTransferQueue;
 
 public class BOJ_16234 {
 
@@ -12,16 +11,18 @@ public class BOJ_16234 {
     public static int[] dy = {0, 0, -1, 1};
 
     public static int N, L, R;
+    public static int sum  = 0;
     public static int Time = 0;
+    public static boolean flag = true;
 
     public static int[][] map;
     public static boolean[][] visited;
 
     public static Queue<int []> q;
+    public static Queue<int []> union;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -39,12 +40,54 @@ public class BOJ_16234 {
             }
         }
 
-        bfs(0, 0);
+        int average;
+
+        do {
+            flag    = true;
+            union   = new LinkedList<>();
+            visited = new boolean[N][N];
+
+
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (!visited[i][j]) {
+                        union = bfs(i, j);
+
+                        if(union.size() == 1) continue;
+
+                        average = sum/union.size();
+
+                        while (!union.isEmpty()) {
+                            map[union.peek()[0]][union.peek()[1]] = average;
+                            union.poll();
+                        }
+                        sum = 0;
+                    }
+                }
+            }
+
+            if(!flag) {
+                Time++;
+            }
+
+        } while (!flag);
+
+        System.out.println(Time);
+
+        br.close();
     }
 
-    public static void bfs(int x, int y) {
+    public static Queue<int []> bfs(int x, int y) {
+
+        Queue<int []> uni = new LinkedList<>();
+
         q = new LinkedList<>();
         q.offer(new int[] {x, y});
+        uni.add(new int[] {x, y});
+
+        sum = map[x][y];
+
+        visited[x][y] = true;
 
         while (!q.isEmpty()) {
             int nx = q.peek()[0];
@@ -57,11 +100,19 @@ public class BOJ_16234 {
                 int cury = ny + dy[i];
 
                 if(curx < 0 || cury < 0 || curx >= N || cury >= N
+                            || visited[curx][cury]
                             || !isOpen(map[nx][ny], map[curx][cury])) continue;
 
                 q.offer(new int[] {curx, cury});
+                uni.add(new int[] {curx, cury});
+
+                visited[curx][cury] = true;
+                sum += map[curx][cury];
+                flag = false;
             }
         }
+
+        return uni;
     }
     public static boolean isOpen(int a, int b) {
         int diff = Math.abs(a-b);
