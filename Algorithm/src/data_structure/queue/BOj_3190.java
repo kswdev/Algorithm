1abe4a02617fa15d36e2971ff48ef4740d87ea33
jogony <seonwongim@gmail.com>
2023-12-29 00:10:29 +0900

    BOJ_3190 - 뱀 게임(작성중)

--- Algorithm/src/data_structure/queue/BOj_3190.java
+++ Algorithm/src/data_structure/queue/BOj_3190.java
@@ -3,21 +3,25 @@ package data_structure.queue;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class BOj_3190 {
 
+    private static int[] dx = {0, 0, -1, 1};
+    private static int[] dy = {1, -1, 0, 0};
     private static int N, K, L;
     private static int[][] map;
-    private static int[][] dir;
+    private static int[][] snake;
+    private static Map<Integer, String> dir = new LinkedHashMap<>();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
         K = Integer.parseInt(br.readLine());
 
-        map = new int[N+1][N+1];
-        dir = new int[N+1][N+1];
+        snake = new int[N+1][N+1];
+        map   = new int[N+1][N+1];
 
         StringTokenizer st;
 
@@ -31,7 +35,42 @@ public class BOj_3190 {
         L = Integer.parseInt(br.readLine());
 
         for (int j = 0; j < L; j++) {
+            st = new StringTokenizer(br.readLine());
 
+            int key    = Integer.parseInt(st.nextToken());
+            String val = st.nextToken();
+            dir.put(key, val);
         }
+
+
+        snake[0][0] = 1;
+
+        gamestart(0, 0);
+    }
+
+    private static void gamestart(int x, int y) {
+        int curx = x;
+        int cury = y;
+        int dirIndex = 0;
+        int time = 1;
+
+        Iterator<Integer> key = dir.keySet().iterator();
+
+        while (key.hasNext()) {
+            int keyTime = key.next();
+            String direction = dir.get(keyTime);
+
+            for (int i = 0; i < keyTime; i++) {
+                curx += dx[dirIndex];
+                cury += dy[dirIndex];
+
+
+            }
+        }
+    }
+
+    private static boolean gameover(int time) {
+
+        return false;
     }
 }
