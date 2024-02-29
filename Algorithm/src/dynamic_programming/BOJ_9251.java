e7010732d89ce66a50a24eba531d7d2318e122b3
koviKsw <seonwongim@gmail.com>
2024-02-29 18:21:21 +0900

    dp 9251 미완

--- Algorithm/src/dynamic_programming/BOJ_9251.java
+++ Algorithm/src/dynamic_programming/BOJ_9251.java
@@ -6,14 +6,41 @@ import java.io.InputStreamReader;
 
 public class BOJ_9251 {
 
-	private static int[][] dp;
-	
+	private static int[][] map;
+	private static int[] dp;
+	private static char[] input;
+	private static char[] input2;
+	private static int N,M;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		String input  = br.readLine();
-		String input2 = br.readLine();
-		
-		
+		input  = br.readLine().toCharArray();
+		input2 = br.readLine().toCharArray();
+
+		N = input.length;
+		M = input2.length;
+
+		map = new int[N][M];
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (input[i] == input2[j])
+					map[i][j] = 1;
+			}
+		}
+
+		int count = countLCS();
+		System.out.println(count);
+	}
+
+	private static int countLCS() {
+		for (int i = 1; i < N; i++) {
+			for (int j = 1; j < M; j++) {
+			}
+		}
+
+
+		return map[N-1][M-1];
 	}
 }
