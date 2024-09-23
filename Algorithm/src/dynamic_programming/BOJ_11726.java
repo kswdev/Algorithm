8759adb6c2619de46d9e57dd8ae4a2c5470e6b75
ksw <seonwongim@gmail.com>
2024-09-23 18:51:06 +0900

    데이크스트라 - BOJ_1238

--- Algorithm/src/dynamic_programming/BOJ_11726.java
+++ Algorithm/src/dynamic_programming/BOJ_11726.java
@@ -16,7 +16,7 @@ public class BOJ_11726 {
 
         dp[1] = 1;
 
-        if (N > 2) {
+        if (N >= 2) {
             dp[2] = 2;
             for (int i = 3; i <= N; i++) {
                 dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
