26613e2faeaf74c1cce56e2e7e6c26813b625a01
koviKsw <seonwongim@gmail.com>
2024-09-23 18:01:13 +0900

    DP - BOJ_11726 - 쉬운 DP 문제

--- Algorithm/src/dynamic_programming/BOJ_11726.java
+++ Algorithm/src/dynamic_programming/BOJ_11726.java
@@ -15,9 +15,9 @@ public class BOJ_11726 {
         dp = new int[N+1];
 
         dp[1] = 1;
-        dp[2] = 2;
 
         if (N > 2) {
+            dp[2] = 2;
             for (int i = 3; i <= N; i++) {
                 dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
             }
