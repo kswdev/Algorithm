a0dff91ba9f5165c1574c95042b436029a482140
ksw <seonwongim@gmail.com>
2024-09-24 21:41:06 +0900

    dp - 간단한 LCS 문제

---
+++ Algorithm/src/dynamic_programming/lcs/BOJ_9251.java
@@ -0,0 +1,32 @@
+package dynamic_programming.lcs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_9251 {
+
+    private static String t1;
+    private static String t2;
+
+    private static int[][] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        t1 = br.readLine();
+        t2 = br.readLine();
+
+        dp = new int[t1.length()+1][t2.length()+1];
+
+        for (int i = 1; i <= t1.length(); i++) {
+            for (int j = 1; j <= t2.length(); j++) {
+                if(t1.charAt(i-1) == t2.charAt(j-1))
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                else
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+
+        System.out.println(dp[t1.length()][t2.length()]);
+    }
+}
