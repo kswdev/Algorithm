8c2d2417df32b976a231ec627c17cbfef7a55da5
koviKsw <seonwongim@gmail.com>
2024-10-02 17:34:45 +0900

    DP - 위상정렬 문제 해결

--- Algorithm/src/dynamic_programming/gold3/BOJ_1005.java
+++ Algorithm/src/dynamic_programming/gold3/BOJ_1005.java
@@ -5,62 +5,78 @@ import java.util.*;
 
 public class BOJ_1005 {
 
-    private static int T, N, K, W;
-    private static int[] buildingTime;
+    private static int[] time;
+    private static int[] link;
     private static int[] dp;
-    private static List<ArrayList<Integer>> orderList;
+    private static boolean[][] map;
+    private static List<List<Integer>> orders = new ArrayList<>();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        T = Integer.parseInt(br.readLine());
+        int T = Integer.parseInt(br.readLine());
 
         for (int t = 0; t < T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
-            String[] splitString = br.readLine().split(" ");
+            int N = Integer.parseInt(st.nextToken());
+            int K = Integer.parseInt(st.nextToken());
 
-            N = Integer.parseInt(splitString[0]);
-            K = Integer.parseInt(splitString[1]);
+            dp   = new int[N+1];
+            time = new int[N+1];
+            link = new int[N+1];
+            map  = new boolean[N+1][N+1];
+            orders.add(new ArrayList<>());
 
-            orderList    = new ArrayList<>();
-            dp           = new int[N+1];
-            buildingTime = new int[N+1];
-
-            splitString = br.readLine().split(" ");
+            st = new StringTokenizer(br.readLine());
 
             for (int n = 1; n <= N; n++) {
-                buildingTime[n] = Integer.parseInt(splitString[n-1]);
-                orderList.add(new ArrayList<>());
-                dp[n] = buildingTime[n];
+                time[n] = Integer.parseInt(st.nextToken());
+                dp[n] = time[n];
             }
 
-            orderList.add(new ArrayList<>());
+            for (int k = 1; k <= K; k++) {
+                st = new StringTokenizer(br.readLine());
 
-            for (int k = 0; k < K; k++) {
-                splitString = br.readLine().split(" ");
+                int start = Integer.parseInt(st.nextToken());
+                int end   = Integer.parseInt(st.nextToken());
 
-                int parent = Integer.parseInt(splitString[0]);
-                int child = Integer.parseInt(splitString[1]);
-
-                orderList.get(child).add(parent);
+                map[start][end] = true;
+                link[end]++;
             }
 
-            W = Integer.parseInt(br.readLine());
-            sb.append(dp(W)).append("\n");
+            int W = Integer.parseInt(br.readLine());
+            int result = findShortestRoute(W, N, K);
+            sb.append(result).append("\n");
         }
 
         System.out.println(sb);
         br.close();
     }
 
-    private static int dp(int n) {
+    private static int findShortestRoute(int W, int N, int K) {
+        Queue<Integer> queue = new LinkedList<>();
 
-        if (dp[n] != buildingTime[n])
-            return dp[n];
+        for (int i = 1; i <= N; i++)
+            if (link[i] == 0)
+                queue.add(i);
 
-        for (Integer integer : orderList.get(n))
-            dp[n] = Math.max(dp(integer) + buildingTime[n], dp[n]);
+        while (!queue.isEmpty()) {
+            int building = queue.poll();
 
-        return dp[n];
+            for (int n = 1; n <= N; n++) {
+                if (map[building][n]) {
+                    dp[n] = Math.max(dp[n], dp[building] + time[n]);
+
+                    link[n]--;
+
+                    if (link[n] == 0)
+                        queue.add(n);
+                }
+            }
+        }
+
+        return dp[W];
     }
 }
