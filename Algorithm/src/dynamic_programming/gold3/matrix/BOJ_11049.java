b785c3bd4495841556c68c0f09eb0da7e5709cce
ksw <seonwongim@gmail.com>
2024-10-08 00:45:58 +0900

    dp - BOJ_11049 (미완)

---
+++ Algorithm/src/dynamic_programming/gold3/matrix/BOJ_11049.java
@@ -0,0 +1,23 @@
+package dynamic_programming.gold3.matrix;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 1.방법 두 매트릭스의 합이 가장 작은 순서대로 곱하기
+ *
+ */
+public class BOJ_11049 {
+
+    private int[][] matrix;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int n = 0; n < N; n++) {
+
+        }
+    }
+}
