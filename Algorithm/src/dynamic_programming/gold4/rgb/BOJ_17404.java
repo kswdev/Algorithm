24f420ae1544c4bce25e79c818d32c0e98fe656f
koviKsw <seonwongim@gmail.com>
2024-10-28 16:26:28 +0900

    BOJ_17404 - 첫 번째 집의 색을 정해두고 마지막 집에 첫 번째 집의 색이 안되게끔 강제

--- Algorithm/src/dynamic_programming/gold4/rgb/BOJ_17404.java
+++ Algorithm/src/dynamic_programming/gold4/rgb/BOJ_17404.java
@@ -11,33 +11,50 @@ public class BOJ_17404 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
+        int INF = 1000*1000;
+
         int n = Integer.parseInt(br.readLine());
 
         int[][] colorPrice = new int[n][3];
-        int[][] dp = new int[n][3];
 
         int minPrice = Integer.MAX_VALUE;
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
+            colorPrice[i][0] = Integer.parseInt(st.nextToken());
+            colorPrice[i][1] = Integer.parseInt(st.nextToken());
+            colorPrice[i][2] = Integer.parseInt(st.nextToken());
+        }
 
-            for (int j = 0; j < 3; j++) {
-                colorPrice[i][j] = Integer.parseInt(st.nextToken());
+        for (int c = 0; c < 3; c++) {
+            int[][] dp = new int[n][3];
 
-                if (i <= 1)
-                    dp[i][j] = colorPrice[i][j];
-                else if (i == n-1) {
-                    dp[i][j] = Math.min(dp[i-1][right(j)] + dp[0][left(j)], dp[i-1][left(j)] + dp[0][right(j)]) + colorPrice[i][j];
-                    minPrice = Math.min(dp[i][j], minPrice);
-                } else {
-                    dp[i][j] = Math.min(dp[i-1][right(j)], dp[i-1][left(j)]) + colorPrice[i][j];
+            int[] tempColor = colorPrice[n-1].clone();
+            colorPrice[n-1][c] = INF;
+
+            dp[0][c] = colorPrice[0][c];
+            dp[0][right(c)] = INF;
+            dp[0][left(c)] = INF;
+
+            for (int i = 1; i < n; i++) {
+                dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2]) + colorPrice[i][0];
+                dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2]) + colorPrice[i][1];
+                dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1]) + colorPrice[i][2];
+
+                if (i == n-1) {
+                    minPrice = Math.min(tripleMathMin(dp[i][0], dp[i][1], dp[i][2]), minPrice);
                 }
             }
+
+            colorPrice[n-1] = tempColor;
         }
 
         System.out.println(minPrice);
     }
 
+    private static int tripleMathMin(int a, int b, int c) {
+        return Math.min(a, Math.min(b, c));
+    }
     private static int right(int i) {
         return (i+1) % 3;
     }
