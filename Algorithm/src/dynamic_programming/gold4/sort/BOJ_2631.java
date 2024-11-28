7621eeb968994a2528091eed4a47cfe8a9e3c1b1
koviKsw <seonwongim@gmail.com>
2024-11-28 18:00:19 +0900

    BOJ_9084 - LCS 이용해서 풀기

--- Algorithm/src/dynamic_programming/gold4/sort/BOJ_2631.java
+++ Algorithm/src/dynamic_programming/gold4/sort/BOJ_2631.java
@@ -7,15 +7,14 @@ import java.io.InputStreamReader;
 public class BOJ_2631 {
 
     private static int N;
-    private static int count = 0;
     private static int[] children;
-    private static int[][] dp;
+    private static int[] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
-        dp = new int[N+1][N+1];
+        dp = new int[N+1];
         children = new int[N + 1];
 
         for (int i = 1; i <= N; i++) {
@@ -24,13 +23,20 @@ public class BOJ_2631 {
 
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= i; j++) {
-                if (children[i] <= children[j]) {
-                    dp[i][j] = dp[i-1][j-1] + 1;
-                }
+
             }
         }
 
-
-        System.out.println(count);
+        System.out.println(children.length - dp[N][N]);
     }
 }
+/*
+       3  7  5  2  6  1  4
+    3  1
+    7     2
+    5        3
+    2           1
+    6              3
+    1                 1
+    4                    2
+ */
