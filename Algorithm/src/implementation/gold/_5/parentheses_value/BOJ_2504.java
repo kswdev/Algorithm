e53ab74a12a685d9747cf902f1f65466864c4a63
koviKsw <seonwongim@gmail.com>
2025-08-29 17:59:41 +0900

    BOJ_2504 - 미해결

--- Algorithm/src/implementation/gold/_5/parentheses_value/BOJ_2504.java
+++ Algorithm/src/implementation/gold/_5/parentheses_value/BOJ_2504.java
@@ -3,6 +3,9 @@ package implementation.gold._5.parentheses_value;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
 /*
  * 괄호 '(', ')', '[', ']'
@@ -22,9 +25,70 @@ import java.io.InputStreamReader;
  */
 public class BOJ_2504 {
 
+    private static char openSmallParentheses = '(';
+    private static char closeSmallParentheses = ')';
+    private static char openBigParentheses = '[';
+    private static char closeBigParentheses = ']';
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String parenthesesSequence = br.readLine();
+        String parentheses = br.readLine();
 
+        try {
+            int result = calculateParentheses(parentheses, 0, parentheses.length() - 1);
+            System.out.println(result);
+        } catch (IllegalArgumentException e) {
+            System.out.println(0);
+        }
+    }
+
+    private static int calculateParentheses(String parentheses, int startIndex, int endIndex) {
+        int sum = 0;
+
+        for (int i = startIndex; i < endIndex; i = getMatchingParenthesesEndIndex(parentheses, i) + 1) {
+            if (endIndex == -1)
+                throw new IllegalArgumentException();
+
+            char startParentheses = parentheses.charAt(i);
+
+            if (startParentheses == openSmallParentheses && parentheses.charAt(i+1) == closeSmallParentheses)
+                sum += 2;
+            else if (startParentheses == openBigParentheses && parentheses.charAt(i+1) == closeBigParentheses)
+                sum += 3;
+            else if (startParentheses == openSmallParentheses)
+                sum += 2 * calculateParentheses(parentheses, i + 1, endIndex - 1);
+            else if (startParentheses == openBigParentheses)
+                sum += 3 * calculateParentheses(parentheses, i + 1, endIndex - 1);
+        }
+
+        return sum;
+    }
+
+    private static int getMatchingParenthesesEndIndex(String parentheses, int startIndex) {
+        Stack<Character> stack = new Stack<>();
+        stack.push(parentheses.charAt(startIndex));
+
+        for (int i = startIndex + 1; i < parentheses.length(); i++) {
+
+            char startParentheses = stack.peek();
+
+            if (matching(startParentheses, parentheses.charAt(i)))
+                stack.pop();
+            else
+                stack.push(parentheses.charAt(i));
+
+            if (stack.isEmpty()) return i;
+        }
+
+        return -1;
+    }
+
+    private static boolean matching(char parenthesesA,  char parenthesesB) {
+
+        if (parenthesesA == openSmallParentheses && parenthesesB == closeSmallParentheses) {
+            return true;
+        } else {
+            return parenthesesA == openBigParentheses && parenthesesB == closeBigParentheses;
+        }
     }
 }
