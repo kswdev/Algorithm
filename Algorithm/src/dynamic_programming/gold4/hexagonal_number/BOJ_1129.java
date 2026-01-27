ee3d20cc1a493a07934b9fd6de0c48afc0fa44d1
ksw <seonwongim@gmail.com>
2026-01-28 01:32:57 +0900

    BOJ_1129 - topdown 스택오버플로우

--- Algorithm/src/dynamic_programming/gold4/hexagonal_number/BOJ_1129.java
+++ Algorithm/src/dynamic_programming/gold4/hexagonal_number/BOJ_1129.java
@@ -3,42 +3,46 @@ package dynamic_programming.gold4.hexagonal_number;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BOJ_1129 {
 
-    private static int[] hexagonalNum;
+    private static List<Integer> hexagonalNum = new ArrayList<>();
+    private static int[] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        hexagonalNum = new int[n+1];
-        hexagonalNum[1] = 1;
+        hexagonalNum.add(0);
+        hexagonalNum.add(1);
+        dp = new int[n+1];
 
-        int index = maxHexagonalNumber(n);
+        maxHexagonalNumber(n);
 
-        System.out.println(minHexagonalNumberCount(index, n));
+        System.out.println(minHexagonalNumberCount(n));
     }
 
-    private static int minHexagonalNumberCount(int index, int n) {
-        if (hexagonalNum[index] == n)
-            return 0;
+    private static int minHexagonalNumberCount(int n) {
+        if (dp[n] != 0)
+            return dp[n];
 
-        if (hexagonalNum[index] < n)
-            return minHexagonalNumberCount(index, n - hexagonalNum[index]) + 1;
-        else
-            return minHexagonalNumberCount(index - 1, n);
-    }
+        int min = Integer.MAX_VALUE;
 
-    private static int maxHexagonalNumber(int n) {
-
-        int maxIndex = 1;
-
-        for (int i = 2; hexagonalNum[i-1] < n; i++) {
-            hexagonalNum[i] = hexagonalNum[i-1] + ((i-1) * 4) + 1;
-            maxIndex = i-1;
+        for (int hex : hexagonalNum) {
+            if (hex > n) break;
+            if (hex == 0) continue;
+            min = Math.min(min, minHexagonalNumberCount(n-hex) + 1);
         }
 
-        return maxIndex;
+        return dp[n] = min;
+    }
+
+    private static void maxHexagonalNumber(int n) {
+        for (int i = 2; hexagonalNum.get(i-1) < n; i++) {
+            hexagonalNum.add(hexagonalNum.get(i-1) + ((i-1) * 4) + 1);
+            dp[hexagonalNum.get(i-1)] = 1;
+        }
     }
 }
