8829be085b54c4ecd3ef5c904b3a6ddf565936c3
koviKsw <seonwongim@gmail.com>
2026-02-05 09:47:28 +0900

    BOJ_1234 - 로직 구상중

--- Algorithm/src/dynamic_programming/gold2/christmas_tree/BOJ_1234.java
+++ Algorithm/src/dynamic_programming/gold2/christmas_tree/BOJ_1234.java
@@ -1,5 +1,20 @@
 package dynamic_programming.gold2.christmas_tree;
 
+/*
+ * 문제.
+ *  크리스마스 트리를 만들려고 한다.
+ *  트리는 N 개의 레벨로 이뤄져 있다.
+ *  또 장난감 색 빨강, 파랑, 초록이 있다.
+ *  레벨 K 에는 딱 K 개의 장난감이 있어야 한다.
+ *  각 레벨에 놓으려고 선택한 색이 있으면 그 색의 장난감의 수는 서로 같아야 한다.
+ *  예)
+ *   레벨 3
+ *    (빨강: 2, 파랑 1) X => 빨강 수 != 파랑 수
+ *   레벨 4
+ *    (빨강: 2, 파랑 2) O => 빨강 수 == 파랑 수
+ *
+ */
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -21,31 +36,36 @@ public class BOJ_1234 {
     // 구상:
     // dp에 레벨, 각 RGB 사용 갯수 표현
     // 탑 다운 방식으로 각 레벨을 거슬러 올라가며 경우의 수 곱셈
-    private static void solve(int lv, int r, int g, int b) {
+    private static int solve(int lv, int r, int g, int b) {
 
-        for (int cnt : commonDivisor(lv)) {
-
+        // LV 이 1이고
+        if (lv == 1) {
+            // 남은 장난감 수가 1 보다 많다면 그 수만큼 return
+            return geCount(lv, r, g, b);
         }
+
+        int result = 0;
+
+        // a. 해당 lv 의 배치 개수 덩어리들을 구하고 다음 라운드로 보낸다.
+        // b. 해당 lv 의 조합 가능한 경우의 수와 재귀 호출 결과를 곱한다.
+        // c. a. 의 덩어리 개수만큼 b의 결과들을 합한다.
+        for (int i = 1; i <= 3; i++) {
+            int availableCount = 0;
+            if (lv % i == 0) {
+
+            }
+        }
+
+        return 0;
     }
 
-
-    // 최대 숫자가 10개 밖에 안돼 공약수 찾는 로직은 생략
-    private static int[] commonDivisor(int num) {
-        switch (num) {
-            case 1: return new int[] {1};
-            case 2: return new int[] {1, 2};
-            case 3: return new int[] {1, 3};
-            case 4: return new int[] {1, 2, 4};
-            case 5: return new int[] {1, 5};
-            case 6: return new int[] {1, 2, 3, 6};
-            case 7: return new int[] {1, 7};
-            case 8: return new int[] {1, 2, 4, 8};
-            case 9: return new int[] {1, 3, 9};
-            case 10: return new int[] {1, 2, 5, 10};
-            default: return new int[] {};
-        }
+    private static int geCount(int cnt, int r, int g, int b) {
+        int result = 0;
+        if (r >= cnt) result++;
+        if (g >= cnt) result++;
+        if (b >= cnt) result++;
+        return result;
     }
-
 
     /**
      * dp[color][lv][count]
